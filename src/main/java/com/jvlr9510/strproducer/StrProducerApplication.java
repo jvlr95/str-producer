2fa50a066df95e3e50e96df8ace983efa18db95e
jvlr95 <jvitor.lr@hotmail.com>
2024-01-14 13:29:08 -0300

    Start project producer kafka

---
+++ src/main/java/com/jvlr9510/strproducer/StrProducerApplication.java
@@ -0,0 +1,13 @@
+package com.jvlr9510.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvlr9510/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvlr9510.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
