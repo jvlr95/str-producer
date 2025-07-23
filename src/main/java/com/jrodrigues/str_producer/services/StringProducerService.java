c2f3f3f558bd06d78c70dea5bff6ce29d4722189
jvlr95 <jvitor.lr@hotmail.com>
2025-07-23 01:46:36 -0300

    add verbose logging with partition and offset in kafka sending message

--- src/main/java/com/jrodrigues/str_producer/services/StringProducerService.java
+++ src/main/java/com/jrodrigues/str_producer/services/StringProducerService.java
@@ -4,7 +4,9 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -12,7 +14,14 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).whenComplete((success, error) -> {
+            if (error == null) {
+                int partition = success.getRecordMetadata().partition();
+                long offset = success.getRecordMetadata().offset();
+                log.info("Send message with success: {} to partition {} with offset {}", message, partition, offset);
+            } else {
+                log.error("Error sending message", error);
+            }
+        });
     }
-    
 }
